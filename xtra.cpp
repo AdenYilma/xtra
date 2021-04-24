740e7ad6a1e6d8ed61bf63f903d18c9ff061330a
Your Name <yilmaa@spu.edu>
2021-04-24 16:30:51 -0700

    final(:
    code+screenshot

--- xtra.cpp
+++ xtra.cpp
@@ -11,35 +11,37 @@ int main()
 {
     string house;
     const int SIZE = 20;
-    int houses[] = { 13, 5, 3, 11, 6, 7, 8, 9, 1, 20, 33, 4, 56, 43, 67, 89, 65, 33, 67 };
     int cells[SIZE] = {};
     int cellIter = 0;
     int westpoint = 0; //beginning of road
     int eastpoint = 100; //end of road
 
+
+    //Randomly generate 20 numbers to act as distances from the westpoint to 20 houses
+    srand((unsigned)time(0));
+    int array[SIZE] = {};
     cout << "Unsorted House List of Distances from the Westpoint";
     cout << "\n";
     cout << "------------";
     cout << "\n";
-    for (int i = 0; i < (sizeof(houses) / sizeof(houses[0])); i++) {
-        cout << houses[i];
-        cout << ", ";
+
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = (rand() % (eastpoint-1)) + 1;
+            cout << array[i];
+            cout << ". ";
     }
     cout << "\n";
-    cout << "Numerically Sorted House List of Distances from the Westpoint";
-    cout << "\n";
-    cout << "------------";
-    cout << "\n";
 
-    int elements = (sizeof(houses) / sizeof(houses[0]));
+    int elems = (sizeof(array) / sizeof(array[0]));
 
-    for (int pass = 0; pass < (elements - 1); pass++) {
+    for (int pass = 0; pass < (elems - 1); pass++) {
         bool isSorted = false;
-        for (int index = 0; index < (elements - 1 - pass); index++) {
-            if (houses[index] > houses[index + 1]) {
-                int temp = houses[index];
-                houses[index] = houses[index + 1];
-                houses[index + 1] = temp;
+        for (int index = 0; index < (elems - 1 - pass); index++) {
+            if (array[index] > array[index + 1]) {
+                int temp = array[index];
+                array[index] = array
+                    [index + 1];
+                array[index + 1] = temp;
                 isSorted = false;
             }
         }
@@ -47,33 +49,51 @@ int main()
             break;
         }
     }
-    //print
-    for (int i = 0; i < (sizeof(houses) / sizeof(houses[0])); i++) {
-        cout << houses[i];
-        cout << ", ";
+    cout << "\n";
+    cout << "Numerically Sorted House List of Distances from the Westpoint";
+    cout << "\n";
+    cout << "------------";
+    cout << "\n";
+    for (int i = 0; i < (sizeof(array) / sizeof(array[0])); i++) {
+        cout << array[i];
+        cout << ". ";
     }
     cout << "\n";
 
     int temp = 0;
-    int curCell = houses[0] + 4; //5 
+    int curCell = array[0] + 4; //5 
     cells[0] = curCell;
     cellIter++;
 
-    for (int i = 1; i < (sizeof(houses) / sizeof(houses[0])); i++) {
-        int distanceForward = (houses[i] + 4) - curCell;
-        int distanceBack = (houses[i] - 4) - curCell;
+    for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+        int distanceForward = (array[i] + 4) - curCell;
+        int distanceBack = (array[i] - 4) - curCell;
         if ((distanceForward <= 4 && distanceForward >= 0) || (distanceBack >= -4 && distanceBack <= 0)) {
         }
         else {
-            curCell = houses[i] + 4;
+            curCell = array[i] + 4;
             cells[cellIter] = curCell;
             cellIter++;
         }
     }
+    cout << "\n";
+    cout << "Cell Base List of Distances from the Westpoint";
+    cout << "\n";
     cout << "------------";
     cout << "\n";
-    for (int i = 0; i < (sizeof(houses) / sizeof(houses[0])); i++) {
-        cout << cells[i];
-        cout << ", ";
+    int totalBases = 0;
+    for (int i = 0; i < (sizeof(array) / sizeof(array[0])); i++) {
+        if (cells[i] != 0) {
+            cout << cells[i];
+            cout << ". ";
+            totalBases++;
+        } 
     }
+    cout << "\n";
+    cout << "\n";
+    cout << "Total Bases";
+    cout << "\n";
+    cout << "------------";
+    cout << "\n";
+    cout << totalBases;
 }
